2c1eac766f2def4daed776930c45c7e1f58b84e0
Bryson Nobles <Bryson.Nobles@passioncitychurch.com>
2024-04-15 14:36:49 -0400

    added a Maze class and started building the input actions for the game

--- maze_game.py
+++ maze_game.py
@@ -5,16 +5,17 @@
 ## Must defeat 5 monsters along the way through 5 stages
 import random
 class Player:
-    def __init__(self, name, race, role):
+    def __init__(self, name):
         self.name = name
-        self.race = race
-        self.role = role
+        self.race = ""
+        self.role = ""
         self.health = 0
         self.power = 0
         self.speed = 0
         self.wisdom = 0
         self.stage = 0
     
+    def assign_race(self, race):
         if race == 'human':
             self.health += 6
             self.power = 4
@@ -35,7 +36,8 @@ class Player:
             self.power = 1
             self.speed = 1
             self.wisdom = 1
-        
+    
+    def assign_role(self, role)
         if role == 'wizard':
             self.power += 1
             self.speed += 2
@@ -70,3 +72,34 @@ class Monster:
         self.wisdom = random.randint(0+stage, 3+stage)
 
 # def species
+
+class Maze:
+    def __init__(self):
+        self.stages = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+
+    def assign_paths(self):
+        stages = self.stages
+        for stage in stages.keys:
+            stage.value = random.randint(2,5)
+
+stat_info = '''
+You have four unique stats to track along your journey:
+Health [0-10]
+Power [0-10]
+Speed [0-10]
+Wisdom [0-10]
+'''
+race_info = '''
+There are three race options:
+Human [Health: 6, Power: 4, Speed: 5, Wisdom: 5]
+Dwarf [Health: 7, Power: 6, Speed: 4, Wisdom: 3]
+Elf [Health: 4, Power: 3, Speed: 7, Wisdom: 6]
+'''
+role_info = '''
+There are three role options:
+Wizard [Power: +1, Speed: +2, Wisdom: +3]
+Archer [Power: +2, Speed: +3, Wisdom: +1]
+Warrior [Power: +3, Speed: +1, Wisdom: +2]
+'''
+player_name = input("You have stumbled upon the Maze of Monsters. Please provide your adventurer's name before proceeding into the Maze. ")
+player_race = input("Please choose your race to determine your starting levels for the Maze. Your options: Human []")
