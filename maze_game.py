c4c09c8d9aaa63fdbc2c80c26b048be95c1d8d8c
Bryson Nobles <Bryson.Nobles@passioncitychurch.com>
2024-05-12 15:01:39 -0400

    Balanced the race and roles and ensured that the stages progressed as expected.

--- maze_game.py
+++ maze_game.py
@@ -6,34 +6,38 @@ class Player:
         self.name = name
         self.race = race
         self.role = role
+        self.maxh = 10
+        self.maxp = 10
+        self.maxs = 10
+        self.maxw = 10
         self.stage = 0
         if race == 'Human':
-            self.health = 6
-            self.power = 1
-            self.speed = 5
-            self.wisdom = 5
-        elif race == 'Dwarf':
-            self.health = 7
-            self.power = 6
+            self.health = 5
+            self.power = 3
             self.speed = 4
-            self.wisdom = 3
+            self.wisdom = 4
+        elif race == 'Dwarf':
+            self.health = 6
+            self.power = 4
+            self.speed = 2
+            self.wisdom = 2
         elif race == 'Elf':
             self.health = 4
-            self.power = 3
-            self.speed = 7
-            self.wisdom = 6
+            self.power = 2
+            self.speed = 5
+            self.wisdom = 5
         if role == 'Wizard':
             self.power += 1
-            self.speed += 2
-            self.wisdom += 3
-        elif role == 'Archer':
-            self.power += 2
-            self.speed += 3
-            self.wisdom += 1
-        elif role == 'Warrior':
-            self.power += 3
             self.speed += 1
             self.wisdom += 2
+        elif role == 'Archer':
+            self.power += 1
+            self.speed += 2
+            self.wisdom += 1
+        elif role == 'Warrior':
+            self.power += 2
+            self.speed += 1
+            self.wisdom += 1
 
     def upgrade(self, stat, value):
         if stat == "Power":
@@ -92,14 +96,14 @@ Speed [0-10]
 Wisdom [0-10]'''
 race_info = '''
 There are three race options:
-Human [Health: 6, Power: 4, Speed: 5, Wisdom: 5]
-Dwarf [Health: 7, Power: 6, Speed: 4, Wisdom: 3]
-Elf [Health: 4, Power: 3, Speed: 7, Wisdom: 6]'''
+Human [Health: 5, Power: 3, Speed: 4, Wisdom: 4]
+Dwarf [Health: 6, Power: 4, Speed: 2, Wisdom: 2]
+Elf [Health: 4, Power: 2, Speed: 5, Wisdom: 5]'''
 role_info = '''
 There are three role options:
-Wizard [Power: +1, Speed: +2, Wisdom: +3]
-Archer [Power: +2, Speed: +3, Wisdom: +1]
-Warrior [Power: +3, Speed: +1, Wisdom: +2]'''
+Wizard [Power: +1, Speed: +1, Wisdom: +2]
+Archer [Power: +1, Speed: +2, Wisdom: +1]
+Warrior [Power: +2, Speed: +1, Wisdom: +1]'''
 print(stat_info)
 print(race_info)
 print(role_info)
@@ -122,19 +126,20 @@ player_name = input("Choose your name: ")
 player = Player(player_name, player_race, player_role)
 # player.assign_race(player_race)
 # player.assign_role(player_role)
-player.stage = 1
+player.stage = 0
 next_stage = 1
 options = 1
 maze = Maze()
 
-# Ask for input to enter into stage one
+#• Ask for input to enter into stage one
 stats = f"Health: {player.health} / Power: {player.power} / Speed: {player.speed} / Wisdom: {player.wisdom}"
 print(f"Welcome to the Maze of Monsters, {player.name} the {player.race} {player.role}! Your current stats are: {stats}")
 enter = input("To enter the first stage of the Maze of Monsters, please type 'Enter' ")
 while enter != 'Enter':
     enter = input("Please type 'Enter' to begin the journey into stage one of the maze. ")
 print("-----")
-# Looping through each of the 5 stages of the maze
+player.stage += 1
+#• Looping through each of the 5 stages of the maze
 if player.stage > len(maze.stages):
         print("You have successfully completed the Maze of Monsters! Go to bed...")
 else:
@@ -146,21 +151,21 @@ else:
             break
         else:
             stats = f"Health: {player.health} / Power: {player.power} / Speed: {player.speed} / Wisdom: {player.wisdom}"
-            player.stage = next_stage
+            # player.stage = next_stage
             print(f"You've made it to stage {player.stage} and your current stats are: {stats}.")
             print("~~~~~")
             paths = maze.display_paths(player.stage)
-            # Prompt for stage path splits
+            #• Prompt for stage path splits
             path_choice = input(f"You have stumbled upon a split in the maze. You must choose one of the following paths: {paths} ") 
             while path_choice not in paths:
                 path_choice = input("Looks like you've tried to go down an imaginary path. Please choose an option from the provided paths above. ")
-            next_stage += 1
+            # next_stage += 1
             paths.pop(paths.index(path_choice))
             options = len(paths)
 
-            # Monster encounter after choosing a path
+            #• Monster encounter after choosing a path
             monster = Monster(player.stage)
-            if monster.species == "empty":
+            if str(monster.species) == "empty":
                 print("Wow! You found a clear path and can move ahead to the next stage easy peasy. *Gain 1 Health*")
                 player.health += 1
             else:
@@ -214,12 +219,11 @@ else:
                                     print("You have not entered a numerical value.")
                                     continue
                 else:
-                    player.stage -= 1
                     if player.stage == 1:
-                        next_stage = player.stage
                         if options == 0:
                             break
                         else:
+                            player.stage -= 1
                             if player.speed > monster.speed:
                                 print("You escaped to the previous stage! You'll need to choose a new path.")
                             elif player.wisdom > monster.wisdom:
@@ -229,7 +233,7 @@ else:
                                 print(f"Somehow you escaped, but the monster did some damage and you lost {monster.power} health.")
                                 player.health -= monster.power
                     else:
-                        next_stage -= 1
+                        player.stage -= 1
                         if options == 0:
                             break
                         else:
